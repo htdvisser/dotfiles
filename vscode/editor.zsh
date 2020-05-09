133b8086e0dd42feeebb6290e73a130fb6d634f9
Hylke Visser <git@htdvisser.nl>
2020-05-09 16:43:21 +0200

    vscode: Add all

---
+++ vscode/editor.zsh
@@ -0,0 +1,6 @@
+function e() {
+  target="${1:-$(pwd)}"
+  workspace="$target/$(basename "$target").code-workspace"
+  if [[ -f "$workspace" ]]; then target="$workspace"; fi
+  code "$target"
+}
---
+++ vscode/install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+dotpath=$(dirname $(readlink -f "$0"))
+. "$dotpath/../util.sh"
+
+if [ $(uname -s) = "Darwin" ]; then
+  code_user_dir="$HOME/Library/Application Support/Code/User"
+else
+  code_user_dir="$HOME/.config/Code/User"
+fi
+
+if [ ! -d "$code_user_dir" ]; then
+  mkdir -p "$code_user_dir"
+fi
+
+create_link "$dotpath/settings.json" "$code_user_dir/settings.json"
+create_link "$dotpath/snippets" "$code_user_dir/snippets"
+
+if command_exists code; then
+  # Install extensions from `code --list-extensions`
+  modules="
+    2gua.rainbow-brackets
+    AndrsDC.base16-themes
+    dbaeumer.vscode-eslint
+    eamodio.gitlens
+    EditorConfig.EditorConfig
+    esbenp.prettier-vscode
+    GitHub.vscode-pull-request-github
+    ms-python.python
+    ms-vscode.Go
+    samverschueren.final-newline
+    streetsidesoftware.code-spell-checker
+    wayou.vscode-todo-highlight
+    zxh404.vscode-proto3
+  "
+  for module in $modules; do
+    code --install-extension "$module" || true
+  done
+else
+  info "code command not found. Skip installing extensions"
+fi
