55946d54e2067e3e217ffc29182146f46ca6b626
Hylke Visser <git@htdvisser.nl>
2020-05-09 16:44:37 +0200

    system: Add empty setup scripts

---
+++ system/setup-macos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+dotpath=$(dirname $(readlink -f "$0"))
+. "$dotpath/../util.sh"
+
+info "installing macOS system"
+
+# TODO: Install and set up stuff.
+
+success "installed macOS system"
