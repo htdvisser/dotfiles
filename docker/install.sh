a9be9e9b2bb0dbd6d10297d9abfc7f2727a48cb1
Hylke Visser <git@htdvisser.nl>
2020-05-09 21:47:59 +0200

    docker: Make install sh compatible

--- docker/install.sh
+++ docker/install.sh
@@ -10,52 +10,49 @@ else
   curl -sSL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker -o "$dotpath/completion.zsh"
 fi
 
-images=(
-  alpine
-  alpine:3.11
-  alpine:3.10
-  alpine:3.9
-  alpine:3.8
-  amazonlinux:2
-  debian
-  debian:10
-  debian:9
-  debian:8
-  golang
-  golang:1.14
-  golang:1.13
-  node
-  node:14
-  node:12
-  node:10
-  mongo
-  mongo:4
-  mongo:3
-  nginx:mainline
-  nginx:stable
-  postgres
-  postgres:12
-  postgres:11
-  postgres:10
-  redis
-  redis:6
-  redis:5
-  redis:4
-  redis:3
-  ubuntu
-  ubuntu:20.04
-  ubuntu:18.04
-  ubuntu:16.04
-  cockroachdb/cockroach
-)
+pull_image() {
+  info "pulling $1"
+  docker pull "$1"
+}
 
 if command_exists docker; then
   if docker info > /dev/null 2>&1; then
-    info "pulling ${#images[*]} Docker images"
-    for image in ${images[*]}; do
-      info "pulling $image"
-      docker pull "$image"
-    done
+    pull_image "alpine"
+    pull_image "alpine:3.11"
+    pull_image "alpine:3.10"
+    pull_image "alpine:3.9"
+    pull_image "alpine:3.8"
+    pull_image "amazonlinux:2"
+    pull_image "debian"
+    pull_image "debian:10"
+    pull_image "debian:9"
+    pull_image "debian:8"
+    pull_image "golang"
+    pull_image "golang:1.14"
+    pull_image "golang:1.13"
+    pull_image "node"
+    pull_image "node:14"
+    pull_image "node:12"
+    pull_image "node:10"
+    pull_image "mongo"
+    pull_image "mongo:4"
+    pull_image "mongo:3"
+    pull_image "nginx:mainline"
+    pull_image "nginx:stable"
+    pull_image "postgres"
+    pull_image "postgres:12"
+    pull_image "postgres:11"
+    pull_image "postgres:10"
+    pull_image "redis"
+    pull_image "redis:6"
+    pull_image "redis:5"
+    pull_image "redis:4"
+    pull_image "redis:3"
+    pull_image "ubuntu"
+    pull_image "ubuntu:20.04"
+    pull_image "ubuntu:18.04"
+    pull_image "ubuntu:16.04"
+    pull_image "cockroachdb/cockroach"
   else
     info "docker not active. Skip pulling images"
   fi
