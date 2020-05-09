a3791b997b89984d9283a2f989d8112115ae6444
Hylke Visser <git@htdvisser.nl>
2020-05-09 15:56:39 +0200

    xdg: Add all

---
+++ xdg/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+dotpath=$(dirname $(readlink -f "$0"))
+
+. "$dotpath/xdg.zsh"
+
+if [ ! -d "$XDG_DATA_HOME" ]; then mkdir -p "$XDG_DATA_HOME"; fi
+if [ ! -d "$XDG_CONFIG_HOME" ]; then mkdir -p "$XDG_CONFIG_HOME"; fi
+if [ ! -d "$XDG_CACHE_HOME" ]; then mkdir -p "$XDG_CACHE_HOME"; fi
