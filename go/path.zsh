0dc76051ab62b18869301f0655f24dd7bd1b9c23
Hylke Visser <hi@htdvisser.dev>
2020-12-26 17:43:45 +0100

    Use go1.16beta1 SDK

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,12 @@
 export GOPATH="$HOME/go"
+
+go_sdk=go1.16beta1
+
+if command -v $go_sdk >/dev/null 2>&1; then
+  export GOROOT="$($go_sdk env GOROOT)"
+  export PATH="$GOROOT/bin:$PATH"
+fi
+
+unset go_sdk
+
 export PATH="$GOPATH/bin:$PATH"
