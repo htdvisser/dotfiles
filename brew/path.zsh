c2e45a432cbd1c83a6545b4b140e2e7d7348e817
Hylke Visser <hi@htdvisser.dev>
2020-12-26 17:43:41 +0100

    Move brew path overrides to brew folder

--- brew/path.zsh
+++ brew/path.zsh
@@ -3,3 +3,18 @@ if [ "$(uname -s)" = "Linux" ]; then
     eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
   fi
 fi
+
+if command -v brew >/dev/null 2>&1; then
+  brew_prefix=$(brew --prefix)
+
+  # Use GNU core utils instead of macOS core utils.
+  [[ -d "$brew_prefix/opt/coreutils/libexec/gnubin" ]] && export PATH="$brew_prefix/opt/coreutils/libexec/gnubin:$PATH"
+
+  # Use GNU sed instead of macOS (BSD) sed.
+  [[ -d "$brew_prefix/opt/gnu-sed/libexec/gnubin" ]] && export PATH="$brew_prefix/opt/gnu-sed/libexec/gnubin:$PATH"
+
+  # Use OpenSSL instead of macOS LibreSSL.
+  [[ -d "$brew_prefix/opt/openssl/bin" ]] && export PATH="$brew_prefix/opt/openssl/bin:$PATH"
+
+  unset brew_prefix
+fi
--- macos/path.zsh
+++
@@ -1,8 +0,0 @@
-# Use GNU core utils instead of macOS core utils.
-[[ -d /usr/local/opt/coreutils/libexec/gnubin ]] && export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-
-# Use GNU sed instead of macOS (BSD) sed.
-[[ -d /usr/local/opt/gnu-sed/libexec/gnubin ]] && export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-
-# Use OpenSSL instead of macOS LibreSSL.
-[[ -d /usr/local/opt/openssl/bin ]] && export PATH="/usr/local/opt/openssl/bin:$PATH"
