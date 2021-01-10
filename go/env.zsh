feb7c7c2851208f6f93eedf1be392686fa81cf94
Hylke Visser <hi@htdvisser.dev>
2021-01-10 12:52:29 +0100

    Revert to proxy.golang.org

--- go/env.zsh
+++ go/env.zsh
@@ -1,2 +1,2 @@
-export GOPROXY="http://10.0.0.1:3000,https://proxy.golang.org,direct"
+export GOPROXY="https://proxy.golang.org,direct"
 export GOPRIVATE="htdvisser.dev/*,go.htdvisser.nl/*,github.com/htdvisser/*"
