906f5f505af78b6472013402ef52570f068df12e
Hylke Visser <git@htdvisser.nl>
2021-07-30 21:02:38 +0200

    Stop overriding Go version

--- go/path.zsh
+++ go/path.zsh
@@ -1,12 +1,12 @@
 export GOPATH="$HOME/go"
 
-go_sdk=go1.16.2
+# go_sdk=go1.xx.x
 
-if command -v "$GOPATH/bin/$go_sdk" >/dev/null 2>&1; then
-  export GOROOT="$("$GOPATH/bin/$go_sdk" env GOROOT)"
-  export PATH="$GOROOT/bin:$PATH"
-fi
+# if command -v "$GOPATH/bin/$go_sdk" >/dev/null 2>&1; then
+#   export GOROOT="$("$GOPATH/bin/$go_sdk" env GOROOT)"
+#   export PATH="$GOROOT/bin:$PATH"
+# fi
 
-unset go_sdk
+# unset go_sdk
 
 export PATH="$GOPATH/bin:$PATH"
