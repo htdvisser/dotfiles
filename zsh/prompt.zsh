8d47677b7edff524b6d6d670b4f0bca180e9c11a
Hylke Visser <hi@htdvisser.dev>
2021-08-02 22:15:47 +0200

    Print command duration before prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,18 +1,42 @@
 autoload -Uz add-zsh-hook
 
+typeset -F SECONDS
+
+prompt_preexec() {
+  exec_start=$SECONDS
+}
+
+add-zsh-hook preexec prompt_preexec
+
 prompt_precmd() {
   res=$?
 
+  # Tab title
   print -n '\e]0;'
   [[ -n $SSH_CONNECTION ]] && print -Pn '(%m) '
   print -Pn '%~'
   print -n '\a'
 
   print ''
+
+  # Exit code
   if [[ $res != 0 ]]
   then
-    print -Pn "[%F{yellow}$res%f] "
+    print -P "[%F{yellow}$res%f]"
   fi
+
+  # Command duration
+  if [[ $exec_start ]]
+  then
+    typeset -i elapsed
+    elapsed=$(($SECONDS * 1000 - $exec_start * 1000))
+    if [[ $elapsed -gt 500 ]]; then
+      print -P "[%F{cyan}${elapsed}ms%f]\a"
+    fi
+    unset exec_start
+  fi
+
+  # Directory
   print -P "%F{blue}%~%f "
 }
 
