70556a85acb6128f09fbb47aa34905119ddea7da
Hylke Visser <git@htdvisser.nl>
2021-08-12 18:38:23 +0200

    Only use GPG agent if installed

--- ssh/agent.zsh
+++ ssh/agent.zsh
@@ -1,2 +1,7 @@
-gpg-connect-agent -q /bye
-export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+if [ command -v gpg-connect-agent ] >/dev/null 2>&1; then
+  gpg-connect-agent -q /bye
+fi
+
+if [ command -v gpg-agent ] >/dev/null 2>&1; then
+  export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+fi
