3146f9bdd349ba88cf54fe0f5be6d8d863dc6ff9
Hylke Visser <git@htdvisser.nl>
2021-08-12 18:40:07 +0200

    Update VSCode extension list

--- vscode/install.sh
+++ vscode/install.sh
@@ -14,6 +14,7 @@ fi
 if [ ! -d "$code_user_dir" ]; then
   mkdir -p "$code_user_dir"
 fi
+
 if [ ! -d "$code_insiders_user_dir" ]; then
   mkdir -p "$code_insiders_user_dir"
 fi
@@ -28,7 +29,6 @@ if command_exists code; then
   # Install extensions from `code --list-extensions`
   modules="
     2gua.rainbow-brackets
-    AndrsDC.base16-themes
     casualjim.gotemplate
     dbaeumer.vscode-eslint
     eamodio.gitlens
@@ -37,16 +37,14 @@ if command_exists code; then
     GitHub.github-vscode-theme
     GitHub.vscode-pull-request-github
     golang.go
-    hashicorp.terraform
     ms-azuretools.vscode-docker
     ms-python.python
     ms-python.vscode-pylance
-    rust-lang.rust
     samverschueren.final-newline
     streetsidesoftware.code-spell-checker
     streetsidesoftware.code-spell-checker-dutch
-    svelte.svelte-vscode
     wayou.vscode-todo-highlight
+    wholroyd.HCL
     zxh404.vscode-proto3
   "
   for module in $modules; do
