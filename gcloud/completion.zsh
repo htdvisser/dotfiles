29b1b6c9637704fff9d5f66f824fcf5d388d61ed
Hylke Visser <git@htdvisser.nl>
2021-08-12 18:37:28 +0200

    Remove gcloud

--- gcloud/completion.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc ]]; then
-  source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
-fi
--- gcloud/path.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc ]]; then
-  source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
-fi
