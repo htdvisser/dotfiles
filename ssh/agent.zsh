9f353b6336a2a20ee2023c47dcb5e7afad797f68
Hylke Visser <hi@htdvisser.dev>
2021-11-16 21:42:55 +0100

    Use YubiKey-Agent for SSH

--- ssh/agent.zsh
+++ ssh/agent.zsh
@@ -1,7 +1,1 @@
-if [ command -v gpg-connect-agent ] >/dev/null 2>&1; then
-  gpg-connect-agent -q /bye
-fi
-
-if [ command -v gpg-agent ] >/dev/null 2>&1; then
-  export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
-fi
+export SSH_AUTH_SOCK="$(brew --prefix)/var/run/yubikey-agent.sock"
