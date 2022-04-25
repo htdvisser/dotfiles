1535e1821cc4b52e085629734c75b9eb5d223bed
Hylke Visser <hi@htdvisser.dev>
2022-04-25 21:54:40 +0200

    Prefer Secretive agent over GPG agent

--- ssh/agent.zsh
+++ ssh/agent.zsh
@@ -1,10 +1,5 @@
-if [[ $(command -v gpg) ]] && [[ $(gpg --card-status) ]] > /dev/null 2>&1; then
-  gpg-connect-agent -q /bye
-  export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-elif [[ $(command -v ykman) ]] && [[ ! -z "$(ykman list)" ]] && [[ -e "$(brew --prefix)/var/run/yubikey-agent.sock" ]]; then
-  export SSH_AUTH_SOCK="$(brew --prefix)/var/run/yubikey-agent.sock"
-elif [[ -e "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh" ]]; then
+if [[ -e "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh" ]]; then
   export SSH_AUTH_SOCK="$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh"
-elif [[ -e "$HOME/.sekey/ssh-agent.ssh" ]]; then
-  export SSH_AUTH_SOCK="$HOME/.sekey/ssh-agent.ssh"
+elif [[ $(command -v gpg) ]] && [[ $(gpg --card-status) ]] > /dev/null 2>&1; then
+  export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
 fi
