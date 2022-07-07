f56baa485a9f996f7cd473c881e17d517bfee854
Hylke Visser <hi@htdvisser.dev>
2022-07-07 09:49:06 +0200

    Use 1Password as default SSH agent

--- ssh/agent.zsh
+++
@@ -1,5 +0,0 @@
-if [[ -e "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh" ]]; then
-  export SSH_AUTH_SOCK="$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh"
-elif [[ $(command -v gpg) ]] && [[ $(gpg --card-status) ]] > /dev/null 2>&1; then
-  export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-fi
