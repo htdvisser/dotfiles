8940ad2efa13bb34ee859d7b74c46b1a7f446ed9
Hylke Visser <hi@htdvisser.dev>
2023-04-07 09:16:21 +0200

    Update go version tooling

--- workspaces/shared/run/before/create_home_dirs.zsh
+++ workspaces/shared/run/before/create_home_dirs.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 mkdir -p $HOME/.aws
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/direnv/lib
 mkdir -p $HOME/.zsh
 mkdir -p $HOME/.zshrc.d
 mkdir -p $HOME/bin
---
+++ workspaces/shared/symlinks/direnv/.config/direnv/lib/use_go.sh
@@ -0,0 +1,21 @@
+use_go() {
+  version=$1
+
+  if [[ -z "$version" ]]; then
+    echo "Usage: use_go [VERSION]"
+    return 1
+  fi
+
+  version=${version#"go"}
+
+  version=$(semver_search "$HOME/sdk" "go" "$version")
+
+  if [[ ! -e "$GOBIN/go${version}" || ! -e "$HOME/sdk/go${version}" ]]; then
+    echo "Go ${version} is not installed."
+    echo "Please run: install_go_sdk ${version}"
+    return 1
+  fi
+
+  export GOROOT="$HOME/sdk/go$1"
+  export PATH="$GOROOT/bin:$PATH"
+}
